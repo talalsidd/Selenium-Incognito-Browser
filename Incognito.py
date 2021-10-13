8e6f163c5b1966033a6e96cd16627de37194b758
Talal Siddiqui <35397160+talalsidd@users.noreply.github.com>
2021-10-14 00:38:13 +0500

    Create Incognito.py
    The way for selenium chrome driver to use incognito mode.

---
+++ Incognito.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.maximize_window()
+driver.get('https://google.com')
+time.sleep(3)
+driver.close()
+
+        
+            
+          
+           
+            
+            
+            
+            
+
+            
+                        
+         
+        
+
+   
+
+
+
