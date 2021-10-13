43695645904c018bc4a160f319c59173c67b0543
Talal Siddiqui <35397160+talalsidd@users.noreply.github.com>
2021-10-14 00:48:12 +0500

    Update Incognito.py

--- Incognito.py
+++ Incognito.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
